b0aab2a813019ec17c33a52415c2d21aec62d5a5
saad760 <saadnbros@hotmail.com>
2022-09-30 09:56:10 -0700

    init

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-#from curses.ascii import isdigit
-#from sre_constants import MAX_UNTIL
-from multiprocessing import current_process
 import random
-import symbol
 
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
 ROWS = 3
-CLOS = 3
+COLS = 3
 
 symbol_count = {
     "A": 2,
@@ -20,12 +16,12 @@ symbol_count = {
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol.items():
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
     columns = []
-    for col in range(cols):
+    for _ in range(cols):
         column = []
         current_symbols = all_symbols[:]
         for _ in range(rows):
@@ -33,7 +29,19 @@ def get_slot_machine_spin(rows, cols, symbols):
             current_symbols.remove(value)
             column.append(value)
 
-        column.append(column)
+        columns.append(column)
+
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+
+        print()
 
 def deposit():
     while True:
@@ -75,7 +83,7 @@ def get_bet():
         else:
             print("Please enter a number.")
 
-    return 
+    return amount
 
 def main():
     balance = deposit()
@@ -91,7 +99,12 @@ def main():
 
 
 
-    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+    print(
+        f"You are betting ${bet} on ${lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
 
+ 
